f7e4968fe23952e37762430b31b0dc464a6309bb
Jonah <jonah@essopl.in>
2023-03-01 20:44:40 +0530

    dbchange

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    const connect = await mongoose.connect(process.env.CONNECTIONSTRING);
     console.log(
       "Database connect: ",
       connect.connection.host,
