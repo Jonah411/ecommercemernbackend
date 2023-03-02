2f205e362049574cf8137dc0187b6d3411c40822
Jonah <jonah@essopl.in>
2023-03-02 12:29:33 +0530

    wishlist changes

--- routes/wishListRoutes.js
+++ routes/wishListRoutes.js
@@ -6,11 +6,11 @@ const {
   getWishList,
 } = require("../controllers/wishListController");
 
-const validateToken = require("../middleware/validateTokenHandler");
+//const validateToken = require("../middleware/validateTokenHandler");
 
 const router = express.Router();
 
-router.use("/", validateToken);
+//router.use("/", validateToken);
 router.route("/add").post(createWishList);
 router.route("/product/:id").get(getAllWishList);
 router.route("/:user_id/:product_id").get(getWishList);
