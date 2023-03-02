278bad1ee7a28053558a4cf041225954b0dca8c6
Jonah <jonah@essopl.in>
2023-03-02 15:58:51 +0530

    change core netlify

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["https://flourishing-pegasus-8ffc5f.netlify.app/"];
 
 const corsOptions = {
   origin: (origin, callback) => {
